76d21f1e92574df403487c0aef1fc26e33cb01f5
Rostislav Khlebnikov <r.khlebnikov@gmail.com>
2015-02-27 18:14:26 +0000

    Using CTK master

--- CMakeExternals/CTK.cmake
+++ CMakeExternals/CTK.cmake
@@ -15,7 +15,7 @@ if(MITK_USE_CTK)
 
   if(NOT DEFINED CTK_DIR)
 
-    set(revision_tag 9331130f+65420ed0+e57224a2)
+    set(revision_tag 6983eccff0bd161df0329b3a1a33d9f235b6baa0)
     #IF(${proj}_REVISION_TAG)
     #  SET(revision_tag ${${proj}_REVISION_TAG})
     #ENDIF()
@@ -71,8 +71,7 @@ if(MITK_USE_CTK)
       SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}-src
       BINARY_DIR ${proj}-build
       PREFIX ${proj}-cmake
-      URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/CTK_${revision_tag}.tar.gz
-      URL_MD5 0ee6baf7333b3bb53480453311f4a3ee
+      GIT_REPOSITORY "git://github.com/commontk/CTK.git"
       UPDATE_COMMAND ""
       INSTALL_COMMAND ""
       CMAKE_GENERATOR ${gen}
