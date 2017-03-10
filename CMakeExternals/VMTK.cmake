056725e53dab478dbe5ea177022000549cea8496
mmarcan <marija.marcan@gmail.com>
2017-03-10 18:01:32 +0000

    ensuring vmtk superbuild links everything okay

--- CMakeExternals/VMTK.cmake
+++ CMakeExternals/VMTK.cmake
@@ -11,7 +11,7 @@ if(MITK_USE_VMTK)
   if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
     message(FATAL_ERROR "${proj}_DIR variable is defined but corresponds to non-existing directory")
   endif()
-
+  
   if(NOT DEFINED ${proj}_DIR)
     set(additional_cmake_args)
 
@@ -20,12 +20,35 @@ if(MITK_USE_VMTK)
         "-DCMAKE_PROJECT_VTK_VMTK_INCLUDE:FILEPATH=${CMAKE_ROOT}/Modules/CTestUseLaunchers.cmake"
       )
     endif()
+	
+	if(MITK_USE_Python)
+    if(NOT MITK_USE_SYSTEM_PYTHON)
+     list(APPEND proj_DEPENDENCIES Python)
+     set(_vmtk_install_python_dir -DVMTK_INSTALL_PYTHON_MODULE_DIR:FILEPATH=${MITK_PYTHON_SITE_DIR})
+    else()
+      set(_vmtk_install_python_dir -DVMTK_INSTALL_PYTHON_MODULE_DIR:PATH=${ep_prefix}/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/site-packages)
+    endif()
 
+    list(APPEND additional_cmake_args
+         -DVMTK_WRAP_PYTHON:BOOL=ON
+         -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE}
+         -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}
+         -DPYTHON_INCLUDE_DIR2:PATH=${PYTHON_INCLUDE_DIR2}
+         -DPYTHON_LIBRARY:FILEPATH=${PYTHON_LIBRARY}
+         ${_vmtk_install_python_dir}
+        )
+  else()
+    list(APPEND additional_cmake_args
+         -DVMTK_WRAP_PYTHON:BOOL=OFF
+		 )
+  endif()
+  
     ExternalProject_Add(${proj}
       LIST_SEPARATOR ${sep}
-      URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/vtkVmtk-vtk6-90e0233.tar.bz2
-      URL_MD5 c961f891868b7f91676509a9988ad76f
+      URL http://www.isd.kcl.ac.uk/cafa/CRIMSON-superbuild/vtkVmtk-vtk6-CRIMSON.tar.bz2
+      #URL_MD5 c961f891868b7f91676509a9988ad76f
       PATCH_COMMAND ${PATCH_COMMAND} -N -p1 -i ${CMAKE_CURRENT_LIST_DIR}/VMTK.patch
+	  INSTALL_COMMAND ""
       CMAKE_GENERATOR ${gen}
       CMAKE_ARGS
         ${ep_common_args}
@@ -41,7 +64,7 @@ if(MITK_USE_VMTK)
     )
 
     set(${proj}_DIR ${ep_prefix})
-    mitkFunctionInstallExternalCMakeProject(${proj})
+    #mitkFunctionInstallExternalCMakeProject(${proj})
   else()
     mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
   endif()
